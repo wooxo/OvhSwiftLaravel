0f41f7583aed071b928c5f1b4333127bd5cc8788
Louis Laszlo <lflaszlo@wooxo.fr>
2015-07-06 13:31:21 +0200

    comments

--- src/Lflaszlo/Facades/OvhSwiftLaravel.php
+++ src/Lflaszlo/Facades/OvhSwiftLaravel.php
@@ -2,7 +2,17 @@
      namespace Lflaszlo\OvhSwiftLaravel\Facades;
      use Illuminate\Support\Facades\Facade;
 
+     /**
+      * Facade of  OvhSwiftLaravel
+      *
+      * @package Lflaszlo\OvhSwiftLaravel\Facades
+      */
      class OvhSwiftLaravel extends Facade {
+
+         /**
+          * Provide the name of the component
+          * @return string
+          */
          protected static function getFacadeAccessor()
          {
              return 'ovh-swift-laravel';
--- src/Lflaszlo/OvhSwiftLaravel/OvhSwiftLaravel.php
+++ src/Lflaszlo/OvhSwiftLaravel/OvhSwiftLaravel.php
@@ -4,13 +4,39 @@ use OpenCloud\OpenStack;
 use Guzzle\Http\Exception\BadResponseException;
 use Illuminate\Support\Facades\Config;
 
-
+/**
+ * Library OvhSwiftLaravel
+ *
+ * @package Lflaszlo\OvhSwiftLaravel
+ */
 class OvhSwiftLaravel {
+    /**
+     * URL of OVH PCI Endpoint
+     * @var string
+     */
     private $url = 'https://auth.cloud.ovh.net/v2.0';
+
+    /**
+     * OpenStack Connection Entity
+     * @var OpenStack
+     */
     private $client;
+
+    /**
+     * Object Storage Entity
+     * @var \OpenCloud\ObjectStore\Service
+     */
     private $service;
+
+    /**
+     * Container Entity
+     * @var \OpenCloud\ObjectStore\Resource\Container
+     */
     private $container;
 
+    /**
+     * Constructor
+     */
     public function __construct(){
         $this->client = new OpenStack($this->url, array(
             'username' => Config::get('ovh-swift-laravel::config.username'),
@@ -21,12 +47,24 @@ class OvhSwiftLaravel {
         $this->container = $this->service->getContainer(Config::get('ovh-swift-laravel::config.container'));
     }
 
+    /**
+     * Get the content of a file
+     * @param $filename
+     * @return \Guzzle\Http\EntityBody
+     */
     public function fileGet($filename)
     {
         $object = $this->container->getObject($filename);
         return $object->getContent();
     }
 
+    /**
+     * Upload a new file
+     * @param $file
+     * @param string $filename
+     *
+     * @return \OpenCloud\ObjectStore\Resource\DataObject
+     */
     public function filePut($file, $filename = null){
 
         $getPath = null;
@@ -48,9 +86,15 @@ class OvhSwiftLaravel {
             }
         }
 
-        $this->container->uploadObject($filename, fopen($getPath, 'r'));
+        return $this->container->uploadObject($filename, fopen($getPath, 'r'));
     }
 
+    /**
+     * Test the file existence
+     * @param string $filename
+     *
+     * @return bool
+     */
     public function fileExists($filename){
         foreach($this->container->objectList() as $obj){
             if($obj->getName() == $filename){
@@ -60,10 +104,19 @@ class OvhSwiftLaravel {
         return false;
     }
 
+    /**
+     * Get file list
+     * @return \OpenCloud\Common\Collection
+     */
     public function fileList(){
         return $this->container->objectList();
     }
 
+    /**
+     * Delete a file
+     * @param string $filename
+     * @return bool
+     */
     public function fileDelete($filename){
         $object = $this->container->getObject($filename);
         return $object->delete();
